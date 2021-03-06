eebe3c62b80c1285b95459f677441706a2765f62
youjiti <youjiti@youjiti-MacBook-Pro.local>
2021-03-06 21:04:27 +0800

    增加页面跳转

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-适用于所有基于dva构建的前端项目
-光标选中一个dispatch的type字符串（比如： “type: 'marketGoodsModal/list',” 这一行） 右键唤出菜单点击“跳转到models文件”选项 或者 ctrl+m 就会跳转到effects或者reducers的定义处
+## -----
+## 跳转到effects或者reducers的定义处
+此功能适用于所有基于dva构建的前端项目 光标选中一个dispatch的type字符串（比如： “type: 'marketGoodsModal/list',” 这一行） 右键唤出菜单点击“跳转到models文件”选项 或者 ctrl+m 就会跳转到effects或者reducers的定义处
+## -----
+## -----
+唤出命令面板 使用toPage命令 输入页面url 就会跳转到对应页面
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,65 @@
 
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as parser from '@babel/parser';
+import * as traverse from '@babel/traverse';
+
+function compile(code: string, url: String) {
+  const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: ["typescript"]
+  });
+  let find = false
+  const visitor = {
+    Identifier(path: any) {
+      if (path.node.name === 'path' && path.parent.value.value === url) {
+        const parent = path.findParent((path: any) => path.isObjectExpression());
+        const component = (parent.node.properties || []).find((s: any) => s.key.name === 'component')
+        const expression = (component.value.arguments || []).find((s: any) => s.type === 'ArrowFunctionExpression')
+        const comPath = expression.body.arguments[0].value
+        if(comPath){
+          vscode.workspace.findFiles('tsconfig.json', '**​/node_modules/**', 1).then(s=>{
+            if(s){
+              const tsconfig = fs.readFileSync(s[0].path, 'utf-8')
+              const alisePath = JSON.parse(tsconfig).compilerOptions.paths['@/*'][0]
+              let comPaths = vscode.workspace.rootPath + alisePath.substr(1, alisePath.length-3) + comPath.substr(1)
+              if(fs.existsSync(comPaths + '/index.tsx')){
+                comPaths = comPaths + '/index.tsx'
+                find = true
+              }else if(fs.existsSync(comPaths + '.tsx')){
+                comPaths = comPaths + '.tsx'
+                find = true
+              }
+              if(find){
+                vscode.workspace.openTextDocument(comPaths).then((document) => {
+                  const texts = document.getText().split('\n');
+                  let index = texts.findIndex((s: any, i: any) => s.indexOf('export default') >= 0)
+                  if (index === -1) {
+                    index = 0
+                  }
+                  const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index + 1, 0));
+                  vscode.window.showTextDocument(document, {
+                    selection: range,
+                  });
+                })
+              }else{
+                vscode.window.showWarningMessage('页面文件未能成功定位，请手动查找');
+              }
+            }
+          })
+        }
+      }
+    }
+  };
+
+  traverse.default(ast, visitor);
+
+  return find
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
-  let disposable = vscode.commands.registerCommand('tomos.toMo', () => {
+  let disposable_toMo = vscode.commands.registerCommand('tomos.toMo', () => {
     const editor = vscode.window.activeTextEditor;
     const document = editor?.document;
     const selection = editor?.selection;
@@ -12,7 +68,7 @@ export function activate(context: vscode.ExtensionContext) {
     const na = arr[0].indexOf("'") >= 0 ? arr[0].split("'")[1] : arr[0];
     const ds = arr[1].indexOf("'") >= 0 ? arr[1].split("'")[0] : arr[1];
     vscode.workspace.findTextInFiles({ pattern: "namespace: " + "'" + na + "'" }, (s: vscode.TextSearchResult) => {
-      if(s.uri){
+      if (s.uri) {
         vscode.workspace.openTextDocument(s.uri).then((document) => {
           const texts = document.getText().split('\n');
           let index = texts.findIndex((s: any, i: any) => s.indexOf(ds + '(') >= 0)
@@ -25,13 +81,30 @@ export function activate(context: vscode.ExtensionContext) {
             selection: range,
           });
         })
-      }else{
+      } else {
         vscode.window.showWarningMessage('models文件未能成功定位，请手动查找');
       }
     })
   });
 
-  context.subscriptions.push(disposable);
+  let disposable_toPage = vscode.commands.registerCommand('tomos.toPage', async () => {
+    let url = await vscode.window.showInputBox({ placeHolder: '请输入页面url' });
+    if (!url) {
+      vscode.window.showWarningMessage('url不能为空');
+      return;
+    }
+    url = "/" + url;
+    vscode.workspace.findTextInFiles({ pattern: " path: '" + url + "'"}, (s: vscode.TextSearchResult) => {
+      if (s.uri) {
+        const code = fs.readFileSync(s.uri.fsPath, 'utf-8')
+        compile(code, url!)
+      }
+    })
+  });
+
+
+  context.subscriptions.push(disposable_toMo);
+  context.subscriptions.push(disposable_toPage);
 }
 
 // this method is called when your extension is deactivated
