29539c8ab3425d1fbd67d406fb63c04bca335075
youjiti <youjiti@youjiti-MacBook-Pro.local>
2021-03-06 14:53:48 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-光标选中一个dispatch的type字符串 （比如： “type: 'marketGoodsModal/list',” 这一行） 右键唤出菜单 点击“跳转到models文件”选项 就会跳转到effects或者reducers的定义处
+适用于所有基于dva构建的前端项目 光标选中一个dispatch的type字符串 （比如： “type: 'marketGoodsModal/list',” 这一行） 右键唤出菜单 点击“跳转到models文件”选项 就会跳转到effects或者reducers的定义处
--- src/extension.ts
+++ src/extension.ts
@@ -67,36 +67,52 @@ function compile(code: any, fileName: String, na: String, ds: String, sparePath?
 export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand('tomos.toMo', () => {
-    const rootPath = vscode.workspace.rootPath + '/src/config/routers.knowledgePay.ts';
-    const file = fs.readFileSync(rootPath, 'utf-8');
-    let fileName = vscode.window.activeTextEditor?.document.fileName;
-    if (!fileName) {
-      return
-    }
-    if (fileName.split('pages/') && fileName.split('pages/')[1]) {
-      fileName = fileName.split('pages/')[1]
-    }
-    if (fileName.indexOf('index.tsx') != -1) {
-      fileName = fileName.split('/index.tsx')[0]
-    } else {
-      fileName = fileName.split('.tsx')[0]
-    }
-    fileName = "@/pages/" + fileName
+    // const rootPath = vscode.workspace.rootPath + '/src/config/routers.knowledgePay.ts';
+    // const file = fs.readFileSync(rootPath, 'utf-8');
+    // let fileName = vscode.window.activeTextEditor?.document.fileName;
+    // if (!fileName) {
+    //   return
+    // }
+    // if (fileName.split('pages/') && fileName.split('pages/')[1]) {
+    //   fileName = fileName.split('pages/')[1]
+    // }
+    // if (fileName.indexOf('index.tsx') != -1) {
+    //   fileName = fileName.split('/index.tsx')[0]
+    // } else {
+    //   fileName = fileName.split('.tsx')[0]
+    // }
+    // fileName = "@/pages/" + fileName
     const editor = vscode.window.activeTextEditor;
     const document = editor?.document;
     const selection = editor?.selection;
     const text = document?.getText(selection);
-    if (!text) {
-      return
-    }
-    const arr = text.split('/');
+    const arr = text!.split('/');
     const na = arr[0].indexOf("'") >= 0 ? arr[0].split("'")[1] : arr[0];
     const ds = arr[1].indexOf("'") >= 0 ? arr[1].split("'")[0] : arr[1];
-    if (!compile(file, fileName, na, ds)) {
-      if (!compile(file, fileName, na, ds, '@/pages/KnowledgePay/Root')) {
-        compile(file, fileName, na, ds, '@/pages/KnowledgePay/' + fileName.split('KnowledgePay/')[1].split('/')[0], true)
+    vscode.workspace.findTextInFiles({ pattern: "namespace: " + "'" + na + "'" }, (s: any, m: any) => {
+      if(s.uri){
+        vscode.workspace.openTextDocument(s.uri).then((document) => {
+          const texts = document.getText().split('\n');
+          let index = texts.findIndex((s: any, i: any) => s.indexOf(ds + '(') >= 0)
+          if (index === -1) {
+            index = 0
+            vscode.window.showWarningMessage('effects或者reducers未能成功定位，请手动查找');
+          }
+          const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index + 1, 0));
+          vscode.window.showTextDocument(document, {
+            selection: range,
+          });
+        })
+      }else{
+        vscode.window.showWarningMessage('models文件未能成功定位，请手动查找');
       }
-    }
+    })
+
+    // if (!compile(file, fileName, na, ds)) {
+    //   if (!compile(file, fileName, na, ds, '@/pages/KnowledgePay/Root')) {
+    //     compile(file, fileName, na, ds, '@/pages/KnowledgePay/' + fileName.split('KnowledgePay/')[1].split('/')[0], true)
+    //   }
+    // }
   });
 
   context.subscriptions.push(disposable);
